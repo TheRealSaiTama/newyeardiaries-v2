b368e016883809b45b0bf6ae283db8f00ddbc91e
therealsaitama <mansecret490@gmail.com>
2026-06-03 23:23:58 +0530

    feat: replace interactive google map iframe with static location image in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -59,7 +59,7 @@ export function renderFooter(content) {
 
           <div class="nyd-footer__col nyd-footer__map-col">
             <h4 class="nyd-footer__heading">Find Us</h4>
-            <iframe class="nyd-footer__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0!2d77.0!3d28.7!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDQyJzAwLjAiTiA3N8KwMDAnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890" width="100%" height="240" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <img class="nyd-footer__map" src="/images/map-newyeardiary.jpg" width="100%" height="240" style="border:0;object-fit:cover;" alt="New Year Diary location">
           </div>
         </div>
 
